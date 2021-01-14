c0ccba55e3a6cd35fb3318cdfd0767b4c58fdfd1
Axel Neri Lucero <60636862+axelnerilucero@users.noreply.github.com>
2021-01-13 23:52:31 -0600

    Add files via upload

---
+++ AWTintro2/src/awtintro2/AWTintro2.java
@@ -0,0 +1,15 @@
+
+package awtintro2;
+
+import ico.fes.poo.gui.Ventana;
+
+
+public class AWTintro2 {
+
+   
+    public static void main(String[] args) {
+        Ventana ven1 = new Ventana();
+        ven1.setVisible(true);
+    }
+    
+}
---
+++ AWTintro2/src/ico/fes/poo/gui/eventos/EventosRaton.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes.poo.gui.eventos;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ *
+ * @author VICTOR
+ */
+public class EventosRaton implements MouseListener {
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Hola");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+   
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+       
+    }
+    
+    
+}
